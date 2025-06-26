406284b8706dd46b8689d9b3258e64e222de98db
Venkata Ajay Kandregula <jaykan2277@gmail.com>
2025-06-26 17:41:03 +0530

    modified subnets/main.tf

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "pub_sub_1" {
     cidr_block = var.pub_sub_1_cidr_block
     vpc_id = var.vpc_id
     map_public_ip_on_launch = true
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-1"
 
     tags = {
       Name = "public subnet -1"
@@ -13,10 +13,10 @@ resource "aws_subnet" "pub_sub_2" {
     cidr_block = var.pub_sub_2_cidr_block
     vpc_id = var.vpc_id
     map_public_ip_on_launch = true
-    availability_zone = "us-east-1b"
+    availability_zone = "us-east-2"
 
     tags = {
-      Name = "public subnet -2"
+      Name = "public subnet-2"
     }
 }
 
@@ -24,10 +24,10 @@ resource "aws_subnet" "pvt_sub_1" {
     cidr_block = var.pvt_sub_1_cidr_block
     vpc_id = var.vpc_id
     map_public_ip_on_launch = false
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-1"
 
     tags = {
-      Name = "private subnet -1"
+      Name = "private subnet-1"
     }
 }
 
@@ -35,9 +35,9 @@ resource "aws_subnet" "pvt_sub_2" {
     cidr_block = var.pvt_sub_2_cidr_block
     vpc_id = var.vpc_id
     map_public_ip_on_launch = false
-    availability_zone = "us-east-1b"
+    availability_zone = "us-east-1"
 
     tags = {
-      Name = "private subnet -2"
+      Name = "private subnet-2"
     }
 }
